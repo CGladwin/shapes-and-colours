dc1a1f1f82503da9877cba1071860530338c1bdd
CGladwin <calebgladwin@gmail.com>
2025-02-26 16:25:26 -0500

    refactored server.js
    no major changes
    
    TODO: create endpoint to post json payload from react app

--- server/server.ts
+++ server/server.ts
@@ -5,25 +5,26 @@ import dotenv from 'dotenv';
 import {dirname} from 'path';
 import path from 'path';
 import { fileURLToPath } from 'url';
+dotenv.config()
 // import { dot } from 'node:test/reporters';
+const app = express();
+
+// app.set('view engine','ejs') // in case I want to generate dynamic views
+
+const SERVER_PORT = process.env.SERVER_PORT
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const corsOptions  = {
-  origin: ["http://localhost:5173"]
-}
-
-dotenv.config()
-
-const app = express();
-app.use(cors(corsOptions));
-
-const SERVER_PORT = process.env.SERVER_PORT
-
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../") )) // serve static files from React App
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) // for POST/PUT requests
+
+const corsOptions  = {
+  origin: ["http://localhost:5174"]
+}
+app.use(cors(corsOptions));
 
 // Example route
 app.get('/api/data', (req, res) => {
@@ -33,7 +34,8 @@ app.get('/api/data', (req, res) => {
 // Example route
 app.get('/', (req, res) => {
     console.log("server has started")
-    res.json({ message: path.join(__dirname, "../src", "index.html") });
+    res.json({ message: path.join(__dirname, "../", "index.html") });
+    // res.sendFile("index.html"); // doesn't work without building
   });
 
 // Start the server
